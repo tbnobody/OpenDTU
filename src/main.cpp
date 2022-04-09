641f8813fae912d34c5edd09ecf2067febde6398
Thomas Basler <thomas@familie-basler.net>
2022-04-09 11:05:58 +0200

    Apply default code formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
