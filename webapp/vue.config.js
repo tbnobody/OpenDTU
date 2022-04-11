ae204f4ca8577821965244451fd7de7fcf65ace7
Thomas Basler <thomas@familie-basler.net>
2022-04-12 00:19:28 +0200

    webapp: Implemented compression of output files

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  productionSourceMap: false,
+  outputDir: '../data',
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+  pluginOptions: {
+    compression:{
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        deleteOriginalAssets: true,
+        minRatio: 0.8,
+      }
+    }
+  }
 })
