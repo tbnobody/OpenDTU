583bc7f366e3c3271d7d2acc705d7b137174e183
Thomas Basler <thomas@familie-basler.net>
2022-06-16 14:10:14 +0200

    Don't disable interrupts
    Not needed as the flow is more less independent

--- lib/CircularBuffer/CircularBuffer.h
+++ lib/CircularBuffer/CircularBuffer.h
@@ -27,8 +27,8 @@
 #define DISABLE_IRQ noInterrupts()
 #define RESTORE_IRQ interrupts()
 #elif ESP32
-#define DISABLE_IRQ portDISABLE_INTERRUPTS()
-#define RESTORE_IRQ portENABLE_INTERRUPTS()
+#define DISABLE_IRQ ;
+#define RESTORE_IRQ ;
 #else
 #define DISABLE_IRQ       \
     uint8_t sreg = SREG;    \
