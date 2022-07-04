88d29a9b434d55d1619795d86bdbe1e053a6847d
Axel Hinrichs <hinrichs@bitscape.de>
2022-07-05 00:41:23 +0200

    [webapp] HomeView more responsive
    * change <div v-for> into <template v-for> to avoid empty divs for
      nonexisting channels
    
    * cleanup bootstrap grid / flex classes

--- webapp/src/components/partials/InverterChannelInfo.vue
+++ webapp/src/components/partials/InverterChannelInfo.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="col">
-        <div class="card">
-            <div v-if="channelNumber >= 1" class="card-header">String {{ channelNumber }}</div>
-            <div v-if="channelNumber == 0" class="card-header">Phase {{ channelNumber + 1 }}</div>
-            <div class="card-body">
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Property</th>
-                            <th scope="col">Value</th>
-                            <th scope="col">Unit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(property, key) in channelData" :key="`prop-${key}`">
-                            <th scope="row">{{ key }}</th>
-                            <td style="text-align: right">{{ formatNumber(property.v) }}</td>
-                            <td>{{ property.u }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <div class="card">
+        <div v-if="channelNumber >= 1" class="card-header">String {{ channelNumber }}</div>
+        <div v-if="channelNumber == 0" class="card-header">Phase {{ channelNumber + 1 }}</div>
+        <div class="card-body">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Property</th>
+                        <th scope="col">Value</th>
+                        <th scope="col">Unit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(property, key) in channelData" :key="`prop-${key}`">
+                        <th scope="row">{{ key }}</th>
+                        <td style="text-align: right">{{ formatNumber(property.v) }}</td>
+                        <td>{{ property.u }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
