d9982719443ebee150f882159c264dcdd4203d80
Thomas Basler <thomas@familie-basler.net>
2022-09-12 21:04:24 +0200

    Implemented crc16 calculation for DevControlCommand

--- lib/Hoymiles/src/commands/DevControlCommand.cpp
+++ lib/Hoymiles/src/commands/DevControlCommand.cpp
@@ -1,8 +1,16 @@
 #include "DevControlCommand.h"
+#include "crc.h"
 
 DevControlCommand::DevControlCommand(uint64_t target_address, uint64_t router_address)
     : CommandAbstract(target_address, router_address)
 {
     _payload[0] = 0x51;
     _payload[9] = 0x81;
+}
+
+void DevControlCommand::udpateCRC(uint8_t len)
+{
+    uint16_t crc = crc16(&_payload[10], len);
+    _payload[10 + len] = (uint8_t)(crc >> 8);
+    _payload[10 + len + 1] = (uint8_t)(crc);
 }
--- lib/Hoymiles/src/commands/DevControlCommand.h
+++ lib/Hoymiles/src/commands/DevControlCommand.h
@@ -5,4 +5,7 @@
 class DevControlCommand : public CommandAbstract {
 public:
     explicit DevControlCommand(uint64_t target_address = 0, uint64_t router_address = 0);
+
+protected:
+    void udpateCRC(uint8_t len);
 };
