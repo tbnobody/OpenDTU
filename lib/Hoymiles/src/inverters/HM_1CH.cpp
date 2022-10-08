223b7ff3ce0da7dd7d95931551d42d643cb99937
Thomas Basler <thomas@familie-basler.net>
2022-10-08 18:03:56 +0200

    Adjusted serial parsing to detect model
    There are some MI-1500 models which are compatible with the Gen3 protocol.
    The adjustment takes this into account.

--- lib/Hoymiles/src/inverters/HM_1CH.cpp
+++ lib/Hoymiles/src/inverters/HM_1CH.cpp
@@ -5,7 +5,22 @@ HM_1CH::HM_1CH(uint64_t serial)
 
 bool HM_1CH::isValidSerial(uint64_t serial)
 {
-    return serial >= 0x112100000000 && serial <= 0x112199999999;
+    // serial >= 0x112100000000 && serial <= 0x112199999999
+
+    uint8_t preId[2];
+    preId[0] = (uint8_t)(serial >> 40);
+    preId[1] = (uint8_t)(serial >> 32);
+
+    if ((uint8_t)(((((uint16_t)preId[0] << 8) | preId[1]) >> 4) & 0xff) == 0x12) {
+        return true;
+    }
+
+    if ((((preId[1] & 0xf0) == 0x10) || ((preId[1] & 0xf0) == 0x20))
+        && (((preId[0] == 0x10) && (preId[1] == 0x22)) || ((preId[0] == 0x11) && (preId[1] == 0x21)))) {
+        return true;
+    }
+
+    return false;
 }
 
 String HM_1CH::typeName()
