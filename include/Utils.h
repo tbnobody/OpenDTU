f5c627e250190b8d3bc1bcefd4bf813ba33b81ac
Thomas Basler <thomas@familie-basler.net>
2022-10-20 19:35:17 +0200

    Move getChipId to separate class

---
+++ include/Utils.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#include <cstdint>
+
+class Utils {
+public:
+    static uint32_t getChipId();
+};
---
+++ src/Utils.cpp
@@ -0,0 +1,11 @@
+#include "utils.h"
+#include <Esp.h>
+
+uint32_t Utils::getChipId()
+{
+    uint32_t chipId = 0;
+    for (int i = 0; i < 17; i += 8) {
+        chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+    }
+    return chipId;
+}
