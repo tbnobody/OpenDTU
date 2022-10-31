173fe275a69bdbb3bc6ad2ef2a45a2a2564ebca3
stromibaer <peter@stromi.at>
2022-10-31 14:52:44 +0100

    sdfsdf

--- src/MqttVictronPublishing.cpp
+++ src/MqttVictronPublishing.cpp
@@ -46,14 +46,14 @@ void MqttVictronPublishingClass::loop()
             JsonObject serviceObj = serviceDoc.as<JsonObject>();
 
             // Get Current phase
-            uint16_t invphase = config.Inverter[i].CurrentPhase;
-            uint64_t invname = config.Inverter[i].Serial;
+            uint16_t invphase = config.Inverter[i+1].CurrentPhase;
+            String invname = config.Inverter[i+1].Name;
 
             String Vtopic = ("device/HM" + str_serial + "/Status");
             DynamicJsonDocument rootDoc(1024);
             rootDoc[F("clientId")] = "HM" + str_serial;
             rootDoc[F("connected")] = 1;
-            rootDoc[F("version")] = "0.1-L" + String(invphase) + "-" + String(invname);
+            rootDoc[F("version")] = "0.1-L" + String(invphase) + "-" + invname;
             rootDoc[F("services")] = serviceObj;
             JsonObject rootObj = rootDoc.as<JsonObject>();
             String data;
