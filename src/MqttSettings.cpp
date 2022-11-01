0f95b6c312a375d5e9272b57fc1e09f4729f3574
stromibaer <peter@stromi.at>
2022-11-01 10:09:05 +0100

    Find den fehler ned

--- src/MqttSettings.cpp
+++ src/MqttSettings.cpp
@@ -314,14 +314,14 @@ String MqttSettingsClass::getPrefix()
     return Configuration.get().Mqtt_Topic;
 }
 
-void MqttSettingsClass::publish(String subtopic, String payload)
+void MqttSettingsClass::publish(const String& subtopic, const String& payload)
 {
     String topic = getPrefix();
     topic += subtopic;
     mqttClient->publish(topic.c_str(), 0, Configuration.get().Mqtt_Retain, payload.c_str());
 }
 
-void MqttSettingsClass::publishHass(String subtopic, String payload)
+void MqttSettingsClass::publishHass(const String& subtopic, const String& payload)
 {
     String topic = Configuration.get().Mqtt_Hass_Topic;
     topic += subtopic;
