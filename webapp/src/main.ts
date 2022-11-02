7fb30050f6930d127168c4641203421e647ec6f7
Thomas Basler <thomas@familie-basler.net>
2022-11-02 20:39:18 +0100

    webapp: Use scss instead of css
    Enables easier theming in future

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import './scss/styles.scss'
 import "bootstrap"
 
 const app = createApp(App)
