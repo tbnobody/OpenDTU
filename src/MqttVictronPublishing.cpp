b6295e72356ad5b75c47cc7ff51f5f350ba446f0
stromibaer <peter@stromi.at>
2022-11-02 09:14:04 +0100

    Fieldname and value corrected

--- src/MqttVictronPublishing.cpp
+++ src/MqttVictronPublishing.cpp
@@ -34,11 +34,6 @@ void MqttVictronPublishingClass::loop()
         for (uint8_t i = 0; i < Hoymiles.getNumInverters(); i++) {
             auto inv = Hoymiles.getInverterByPos(i);
 
-            //char buffer[sizeof(uint64_t) * 8 + 1];
-            //snprintf(buffer, sizeof(buffer), "%0x%08x",
-            //    ((uint32_t)((inv->serial() >> 32) & 0xFFFFFFFF)),
-            //    ((uint32_t)(inv->serial() & 0xFFFFFFFF)));
-            //String str_serial = String(buffer);
             String str_serial = inv->serialString();
 
             // Publish inverter as device service to Victron Venus OS with connected=1
@@ -109,20 +104,12 @@ void MqttVictronPublishingClass::loop()
 
 void MqttVictronPublishingClass::publishField(std::shared_ptr<InverterAbstract> inv, uint16_t invphase, uint8_t fieldId)
 {
-    String fieldname = (inv->Statistics()->getChannelFieldName(0, fieldId));
-
-    Serial.print(F("Fieldname: "));
-    Serial.print(fieldname);
-    Serial.print(F(" - FieldID: "));
-    Serial.print(fieldId);
-    Serial.print(F(" value: "));
-    Serial.println(double(inv->Statistics()->getChannelFieldValue(0, fieldId)));
-
     bool response = false;
-    if ( fieldname == "Voltage" ) { response = true; }
-    if ( fieldname == "Power") { response = true; }
-    if ( fieldname == "Current" ) { response = true; }
-    if ( fieldname == "YieldTotal") { response = true; }
+    String fieldname;
+    if ( fieldId == 4 ) { response = true; fieldname = "YieldTotal"; }
+    if ( fieldId == 5 ) { response = true; fieldname = "Voltage"; }
+    if ( fieldId == 6 ) { response = true; fieldname = "Current"; }
+    if ( fieldId == 7 ) { response = true; fieldname = "Power"; }
 
     if ( response ) {   
         double fieldval = double(inv->Statistics()->getChannelFieldValue(0, fieldId));
@@ -134,17 +121,11 @@ void MqttVictronPublishingClass::publishField(std::shared_ptr<InverterAbstract> 
         String topic_Victron_sum;
         String topic_Victron_phase;
     
-        //char serial[sizeof(uint64_t) * 8 + 1];
-        //snprintf(serial, sizeof(serial), "%0x%08x",
-        //((uint32_t)((inv->serial() >> 32) & 0xFFFFFFFF)),
-        //((uint32_t)(inv->serial() & 0xFFFFFFFF)));
-        //String invSerial = String(serial);
         String invSerial = inv->serialString();
 
         String deviceInstance = MqttSettings.getVictronDeviceInstance(invSerial);
 
-        // fieldname[0] = std::toupper(fieldname[0]);
-        if ( fieldname == "YieldTotal" ) { 
+        if ( fieldId == 4 ) { 
             topic_Victron_sum += topic + "/" + deviceInstance + "/Ac/Energy/Forward";
             topic_Victron_phase += topic + "/" + deviceInstance + "/Ac/L" + invphase + "/Energy/Forward";
         } else {
@@ -160,8 +141,8 @@ void MqttVictronPublishingClass::publishField(std::shared_ptr<InverterAbstract> 
         serializeJson(valueObj, data);
 
         Serial.print(F("MqTT publish value: "));
-        Serial.print(data);
         Serial.print(fieldId);
+        Serial.print(data);
         Serial.print(F(" to Venus OS with topic sum: "));
         Serial.print(topic_Victron_sum);
         Serial.print(F(" and topic phase: "));
