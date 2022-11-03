f1418d24adddeda1c91791d70f37c94d65d516f4
Thomas Basler <thomas@familie-basler.net>
2022-11-03 21:13:42 +0100

    Password protection for network settings API

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -100,7 +100,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     // redirect to login page if not logged in and trying to access a restricted page
     const publicPages = ['/', '/login', '/about', '/info/network', '/info/system', '/info/ntp', '/info/mqtt',
-        '/settings/network', '/settings/ntp', '/settings/mqtt', '/settings/inverter', '/firmware/upgrade', '/settings/config', ];
+        '/settings/ntp', '/settings/mqtt', '/settings/inverter', '/firmware/upgrade', '/settings/config', ];
     const authRequired = !publicPages.includes(to.path);
     const loggedIn = localStorage.getItem('user');
 
