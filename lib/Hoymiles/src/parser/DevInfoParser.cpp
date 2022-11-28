3c8bd5657699990299a55c39b6fb5592353094c6
Thomas Basler <thomas@familie-basler.net>
2022-11-28 18:16:46 +0100

    Added MI-1500 with Gen3 Protocol to Hardware Id list

--- lib/Hoymiles/src/parser/DevInfoParser.cpp
+++ lib/Hoymiles/src/parser/DevInfoParser.cpp
@@ -18,6 +18,7 @@ const devInfo_t devInfo[] = {
     { { 0x10, 0x11, 0x30, ALL }, 800, "HM-800" },
     { { 0x10, 0x11, 0x40, ALL }, 800, "HM-800" },
     { { 0x10, 0x12, 0x10, ALL }, 1200, "HM-1200" },
+    { { 0x10, 0x02, 0x30, ALL }, 1500, "MI-1500 Gen3" },
     { { 0x10, 0x12, 0x30, ALL }, 1500, "HM-1500" },
     { { 0x10, 0x10, 0x10, 0x15 }, static_cast<uint16_t>(300 * 0.7), "HM-300" }, // HM-300 factory limitted to 70%
 };
