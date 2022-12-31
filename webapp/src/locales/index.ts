ca3349f801cd6d8fccde73c6a20ae34472953321
Thomas Basler <thomas@familie-basler.net>
2022-12-31 11:47:51 +0100

    webapp: Added France translation
    Thanks to @beneva52

--- webapp/src/locales/index.ts
+++ webapp/src/locales/index.ts
@@ -1,19 +1,23 @@
 import en from './en.json'
 import de from './de.json'
+import fr from './fr.json'
 
 export enum Locales {
     EN = 'en',
     DE = 'de',
+    FR = 'fr',
 }
 
 export const LOCALES = [
     { value: Locales.EN, caption: 'English' },
-    { value: Locales.DE, caption: 'Deutsch' }
+    { value: Locales.DE, caption: 'Deutsch' },
+    { value: Locales.FR, caption: 'Français' },
 ]
 
 export const messages = {
-  [Locales.EN]: en,
-  [Locales.DE]: de
+    [Locales.EN]: en,
+    [Locales.DE]: de,
+    [Locales.FR]: fr,
 };
 
 export const defaultLocale = Locales.EN;
