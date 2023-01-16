1147b34e639e41d3e4eb20154291570c3b622bf3
janchri <jans.christoph@gmail.com>
2023-01-16 11:24:25 +0100

    Create new Class Files for Wireguard; improve encapsulation

---
+++ src/WireguardSettings.cpp
@@ -0,0 +1,29 @@
+#include <WireguardSettings.h>
+#include "Configuration.h"
+#include "MessageOutput.h"
+#include "defaults.h"
+
+WireguardSettingsClass::WireguardSettingsClass()
+{
+}
+
+void WireguardSettingsClass::loop(){
+}
+
+void WireguardSettingsClass::init()
+{ // Configure & Start Wireguard Client
+    if (!wg.is_initialized() && Configuration.get().Wg_Enabled) {
+        beginWireGuard();
+    }
+}
+
+void WireguardSettingsClass::beginWireGuard(){
+    wg.begin(
+        Configuration.get().Wg_Local_Ip,           // IP address of the local interface
+        Configuration.get().Wg_Opendtu_Private_Key,// Private key of the local interface
+        Configuration.get().Wg_Endpoint_Address,   // Address of the endpoint peer.
+        Configuration.get().Wg_Endpoint_Public_Key,// Public key of the endpoint peer.
+        Configuration.get().Wg_Endpoint_Port);     // Port pf the endpoint peer.
+}
+
+WireguardSettingsClass WireguardSettings;
