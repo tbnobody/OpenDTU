c67b3e3cbdf944786b53001f98e82e217dc8cc89
Thomas Basler <thomas@familie-basler.net>
2023-02-24 19:10:35 +0100

    Prometheus API: Set correct metrics type

--- include/WebApi_prometheus.h
+++ include/WebApi_prometheus.h
@@ -3,6 +3,7 @@
 
 #include <ESPAsyncWebServer.h>
 #include <Hoymiles.h>
+#include <map>
 
 class WebApiPrometheusClass {
 public:
@@ -15,4 +16,28 @@ private:
     void addField(AsyncResponseStream* stream, String& serial, uint8_t idx, std::shared_ptr<InverterAbstract> inv, ChannelType_t type, ChannelNum_t channel, FieldId_t fieldId, const char* channelName = NULL);
 
     AsyncWebServer* _server;
+
+    enum {
+        METRIC_TYPE_NONE = 0,
+        METRIC_TYPE_GAUGE,
+        METRIC_TYPE_COUNTER,
+    };
+    const char* _metricTypes[3] = { 0, "gauge", "counter" };
+
+    std::map<FieldId_t, uint8_t> _fieldMetricAssignment {
+        { FLD_UDC, METRIC_TYPE_GAUGE },
+        { FLD_IDC, METRIC_TYPE_GAUGE },
+        { FLD_PDC, METRIC_TYPE_GAUGE },
+        { FLD_YD, METRIC_TYPE_COUNTER },
+        { FLD_YT, METRIC_TYPE_COUNTER },
+        { FLD_UAC, METRIC_TYPE_GAUGE },
+        { FLD_IAC, METRIC_TYPE_GAUGE },
+        { FLD_PAC, METRIC_TYPE_GAUGE },
+        { FLD_F, METRIC_TYPE_GAUGE },
+        { FLD_T, METRIC_TYPE_GAUGE },
+        { FLD_PF, METRIC_TYPE_GAUGE },
+        { FLD_EFF, METRIC_TYPE_GAUGE },
+        { FLD_IRR, METRIC_TYPE_GAUGE },
+        { FLD_PRA, METRIC_TYPE_GAUGE }
+    };
 };
