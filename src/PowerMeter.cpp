a091e80ed014dcef2e881c32cd8dd4881d952a26
MalteSchm <malte.schmidt@gmx.de>
2023-03-29 19:07:16 +0200

    deal with un-initialized values

--- src/PowerMeter.cpp
+++ src/PowerMeter.cpp
@@ -27,9 +27,14 @@ void PowerMeterClass::init()
 
     CONFIG_T& config = Configuration.get();
 
-    MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter1, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
-    MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter2, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
-    MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter3, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
+    if (config.PowerMeter_Enabled && 
+        (strlen(config.PowerMeter_MqttTopicPowerMeter1) > 0) &&
+        (strlen(config.PowerMeter_MqttTopicPowerMeter2) > 0) &&
+        (strlen(config.PowerMeter_MqttTopicPowerMeter3) > 0) ) {
+      MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter1, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
+      MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter2, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
+      MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter3, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
+    }
        
     mqttInitDone = true;
 
