82038d3e2d35b7cb488543dcaabc379e44534462
jstammi <jstammi@gmx.net>
2023-06-28 18:57:52 +0200

    Fix: do not calc sizes multiple times
    tbnobody/OpenDTU/issues/1022

--- lib/Hoymiles/src/commands/RealTimeRunDataCommand.cpp
+++ lib/Hoymiles/src/commands/RealTimeRunDataCommand.cpp
@@ -29,11 +29,11 @@ bool RealTimeRunDataCommand::handleResponse(InverterAbstract* inverter, fragment
     // Check if at least all required bytes are received
     // In case of low power in the inverter it occours that some incomplete fragments
     // with a valid CRC are received.
-    if (getTotalFragmentSize(fragment, max_fragment_id) < inverter->Statistics()->getExpectedByteCount()) {
-        Hoymiles.getMessageOutput()->printf("ERROR in %s: Received fragment size: %d min. expected size: %d\r\n",
-            getCommandName().c_str(),
-            getTotalFragmentSize(fragment, max_fragment_id),
-            inverter->Statistics()->getExpectedByteCount());
+    uint8_t fragmentsSize = getTotalFragmentSize(fragment, max_fragment_id);
+    uint8_t expectedSize = inverter->Statistics()->getExpectedByteCount();
+    if (fragmentsSize < expectedSize) {
+        Hoymiles.getMessageOutput()->printf("ERROR in %s: Received fragment size: %d, min expected size: %d\r\n",
+            getCommandName().c_str(), fragmentsSize, expectedSize);
 
         return false;
     }
