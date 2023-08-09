4bf094c3ef01c8f13a5a479e8c31285bd759e25e
Thomas Basler <thomas@familie-basler.net>
2023-08-09 18:23:50 +0200

    webapp: Update default proxy ip

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -14,7 +14,7 @@ let proxy_target;
 try {
     proxy_target = require('./vite.user.ts').proxy_target;
 } catch (error) {
-    proxy_target = '192.168.178.87';
+    proxy_target = '192.168.20.110';
 }
 
 // https://vitejs.dev/config/
