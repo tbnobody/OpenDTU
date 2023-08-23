27d9f73a8d20d582d1f8baa79bcc5cfae5a9a0fd
Martin Dummer <martin.dummer@gmx.net>
2023-08-23 19:56:30 +0200

    Fix: Home Assistan autoconf change inverter limits
    The announced limits for inverters in HA autoconfig messages are not suitable
    for the sub-project OpenDTU-onBattery. Error messages found in HA logs
    are
    
    Invalid value for number.limit_persistent_relative: 0.1 (range 1.0 - 100.0)
    Invalid value for number.limit_nonpersistent_absolute: 0.6 (range 10.0 - 2250.0)
    Invalid value for number.limit_persistent_absolute: 0.6 (range 10.0 - 2250.0)
    Invalid value for number.limit_nonpersistent_absolute: 9.6 (range 10.0 - 2250.0)
    Invalid value for number.limit_persistent_absolute: 9.6 (range 10.0 - 2250.0)
    
    This fix changes the limit minimum to 0 (zero) instead of 1 or 10.
    
    Signed-off-by: Martin Dummer <martin.dummer@gmx.net>

--- include/MqttHandleHass.h
+++ include/MqttHandleHass.h
@@ -59,7 +59,7 @@ private:
     void publish(const String& subtopic, const String& payload);
     void publishField(std::shared_ptr<InverterAbstract> inv, ChannelType_t type, ChannelNum_t channel, byteAssign_fieldDeviceClass_t fieldType, bool clear = false);
     void publishInverterButton(std::shared_ptr<InverterAbstract> inv, const char* caption, const char* icon, const char* category, const char* deviceClass, const char* subTopic, const char* payload);
-    void publishInverterNumber(std::shared_ptr<InverterAbstract> inv, const char* caption, const char* icon, const char* category, const char* commandTopic, const char* stateTopic, const char* unitOfMeasure, int16_t min = 1, int16_t max = 100);
+    void publishInverterNumber(std::shared_ptr<InverterAbstract> inv, const char* caption, const char* icon, const char* category, const char* commandTopic, const char* stateTopic, const char* unitOfMeasure, int16_t min = 0, int16_t max = 100);
     void publishInverterBinarySensor(std::shared_ptr<InverterAbstract> inv, const char* caption, const char* subTopic, const char* payload_on, const char* payload_off);
     void createDeviceInfo(JsonObject& object, std::shared_ptr<InverterAbstract> inv);
 
