68783b450f91232b7e2ef7673a6c138e2849d00d
Bernhard Kirchen <schlimmchen@users.noreply.github.com>
2023-09-04 14:08:30 +0200

    Messages: thread-safety and dynamic memory (#418)
    * thread-safety and dynamic memory for MessageOutput
    
    * use dynamic memory to allow handling of arbitrary message lenghts.
    * keep a message buffer for every task so no task ever mangles the
      message of another task.
    * every complete line is written to the serial console and moved to
      a line buffer for sending them through the websocket.
    * the websocket is always fed complete lines.
    * make sure to feed only as many lines as possible to the websocket
      handler, so that no lines are dropped.
    * lock all MessageOutput state against concurrent access.
    
    * MessageOutput: respect HardwareSerial buffer size
    
    the MessageOutput class buffers whole lines of output printed by any
    task in order to avoid mangling of text. that means we hand over full
    lines to the HardwareSerial instance, which might be too much in one
    call to write(buffer, size). we now check the return value of
    write(buffer, size) and call the function again with the part of the
    message that could not yet be written by HardwareSerial.

--- include/MessageOutput.h
+++ include/MessageOutput.h
@@ -2,11 +2,12 @@
 #pragma once
 
 #include <AsyncWebSocket.h>
-#include <HardwareSerial.h>
-#include <Stream.h>
+#include <Print.h>
+#include <freertos/task.h>
 #include <mutex>
-
-#define BUFFER_SIZE 500
+#include <vector>
+#include <unordered_map>
+#include <queue>
 
 class MessageOutputClass : public Print {
 public:
@@ -16,13 +17,19 @@ public:
     void register_ws_output(AsyncWebSocket* output);
 
 private:
-    AsyncWebSocket* _ws = NULL;
-    char _buffer[BUFFER_SIZE];
-    uint16_t _buff_pos = 0;
-    uint32_t _lastSend = 0;
-    bool _forceSend = false;
+    using message_t = std::vector<uint8_t>;
+
+    // we keep a buffer for every task and only write complete lines to the
+    // serial output and then move them to be pushed through the websocket.
+    // this way we prevent mangling of messages from different contexts.
+    std::unordered_map<TaskHandle_t, message_t> _task_messages;
+    std::queue<message_t> _lines;
+
+    AsyncWebSocket* _ws = nullptr;
 
     std::mutex _msgLock;
+
+    void serialWrite(message_t const& m);
 };
 
 extern MessageOutputClass MessageOutput;
