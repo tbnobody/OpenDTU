ca18d2c8419e3bfcd5c33a046d72baae093f9502
Thomas Basler <thomas@familie-basler.net>
2024-01-04 14:09:58 +0100

    Prevent empty HASS auto discovery topics if memory allocation fails

--- include/Utils.h
+++ include/Utils.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
+#include <ArduinoJson.h>
 #include <cstdint>
 
 class Utils {
@@ -9,4 +10,5 @@ public:
     static uint64_t generateDtuSerial();
     static int getTimezoneOffset();
     static void restartDtu();
+    static bool checkJsonAlloc(const DynamicJsonDocument& doc, const char* function, const uint16_t line);
 };
