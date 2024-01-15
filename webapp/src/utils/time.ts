f26e82424781da8d78f08545170b84b2f8891b69
Nikolaj Kappler <info@nkappler.de>
2024-01-15 22:40:23 +0100

    fix #1649
    one of the goals of my pull request, besides simplifying the code was to have localization.
    
    It's nice that the browser can handle this, but for consistency, we'll go with vue-i18n since it is already available

--- webapp/src/utils/time.ts
+++ webapp/src/utils/time.ts
@@ -1,8 +1,8 @@
-export const timestampToString = (timestampSeconds: number, includeDays = false): string => {
-    const timeString = new Date(timestampSeconds * 1000).toLocaleTimeString([], { timeZone: "UTC" });
-    if (!includeDays) return timeString;
+export const timestampToString = (locale: string, timestampSeconds: number, includeDays = false): string[] => {
+    const timeString = new Date(timestampSeconds * 1000).toLocaleTimeString(locale, { timeZone: "UTC", hour12: false });
+    if (!includeDays) return [timeString];
 
     const secondsPerDay = 60 * 60 * 24;
-    const days = Math.floor(timestampSeconds / secondsPerDay);
-    return new Intl.RelativeTimeFormat().format(-days, "day") + " " + timeString;
+    const days = Math.floor(timestampSeconds / secondsPerDay).toFixed(0);
+    return [days, timeString];
 }
