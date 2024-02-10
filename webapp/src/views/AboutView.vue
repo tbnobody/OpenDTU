b9bf454237273480b09bb3731cfa53b400af71fc
Thomas Basler <thomas@familie-basler.net>
2024-02-10 02:10:06 +0100

    webapp: Add link to documentation in about view

--- webapp/src/views/AboutView.vue
+++ webapp/src/views/AboutView.vue
@@ -2,15 +2,31 @@
     <BasePage :title="$t('about.AboutOpendtu')">
         <div class="accordion" id="accordionExample">
             <div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
+                <h2 class="accordion-header" id="headingDocumentation">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        data-bs-target="#collapseDocumentation" aria-expanded="true" aria-controls="collapseOne">
+                        <span class="badge text-bg-secondary">
+                            <BIconInfoCircle class="fs-4" />
+                        </span>&nbsp;{{ $t('about.Documentation') }}
+                    </button>
+                </h2>
+                <div id="collapseDocumentation" class="accordion-collapse collapse show" aria-labelledby="headingDocumentation"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <p class="fw-normal" v-html="$t('about.DocumentationBody')"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                         <span class="badge text-bg-secondary">
                             <BIconInfoCircle class="fs-4" />
                         </span>&nbsp;{{ $t('about.ProjectOrigin') }}
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         <p class="fw-normal" v-html="$t('about.ProjectOriginBody1')"></p>
