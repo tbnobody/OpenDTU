0bd070b98eca7494d42f7e5c4a10eeef9cf2074c
CreasolTech <psubiaco@creasol.it>
2024-03-12 07:26:47 +0100

    Added Italian language

--- webapp/src/locales/index.ts
+++ webapp/src/locales/index.ts
@@ -4,12 +4,14 @@ export enum Locales {
     EN = 'en',
     DE = 'de',
     FR = 'fr',
+    IT = 'it',
 }
 
 export const LOCALES = [
     { value: Locales.EN, caption: 'English' },
     { value: Locales.DE, caption: 'Deutsch' },
     { value: Locales.FR, caption: 'Français' },
+    { value: Locales.IT, caption: 'Italiano' },
 ]
 
 export const dateTimeFormats: I18nOptions["datetimeFormats"] = {
@@ -36,6 +38,17 @@ export const dateTimeFormats: I18nOptions["datetimeFormats"] = {
         }
     },
     [Locales.FR]: {
+        'datetime': {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour12: false
+        }
+    },
+    [Locales.IT]: {
         'datetime': {
             hour: 'numeric',
             minute: 'numeric',
@@ -84,6 +97,23 @@ export const numberFormats: I18nOptions["numberFormats"] = {
         },
     },
     [Locales.FR]: {
+        decimal: {
+            style: 'decimal',
+        },
+        decimalNoDigits: {
+            style: 'decimal', minimumFractionDigits: 0, maximumFractionDigits: 0
+        },
+        decimalTwoDigits: {
+            style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2
+        },
+        percent: {
+          style: 'percent',
+        },
+        kilobyte: {
+            style: 'unit', unit: 'kilobyte',
+        },
+    },
+    [Locales.IT]: {
         decimal: {
             style: 'decimal',
         },
