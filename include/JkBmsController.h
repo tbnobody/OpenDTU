tbnobody/OpenDTU

commit be41e6b906cf02ab743d5f89626bcbf011f921a8

refactor serial port manager: hand out UARTs FCFS
get rid of particular compile-time designations by UART index. just hand
out the next free index of hardware UARTs, or indicate that none is
available any more.

use names as keys to register and free UARTs.

include/JkBmsController.h
@@ -7,11 +7,11 @@
 #include "Battery.h"
 #include "JkBmsSerialMessage.h"
 
+//#define JKBMS_DUMMY_SERIAL
+
 class DataPointContainer;
 
 namespace JkBms {
-
-uint8_t constexpr HwSerialPort = ((ARDUINO_USB_CDC_ON_BOOT != 1)?2:0);
 
 class Controller : public BatteryProvider {
     public:
@@ -21,9 +21,16 @@ class Controller : public BatteryProvider {
         void deinit() final;
         void loop() final;
         std::shared_ptr<BatteryStats> getStats() const final { return _stats; }
-        int usedHwUart() const final { return HwSerialPort; }
 
     private:
+        static char constexpr _serialPortOwner[] = "JK BMS";
+
+#ifdef JKBMS_DUMMY_SERIAL
+        std::unique_ptr<DummySerial> _upSerial;
+#else
+        std::unique_ptr<HardwareSerial> _upSerial;
+#endif
+
         enum class Status : unsigned {
             Initializing,
             Timeout,

src/JkBmsController.cpp
@@ -5,11 +5,10 @@
 #include "MessageOutput.h"
 #include "JkBmsDataPoints.h"
 #include "JkBmsController.h"
+#include "SerialPortManager.h"
 #include <frozen/map.h>
 
 namespace JkBms {
-
-//#define JKBMS_DUMMY_SERIAL
 
 #ifdef JKBMS_DUMMY_SERIAL
 class DummySerial {
@@ -198,9 +197,6 @@ class DummySerial {
         size_t _msg_idx = 0;
         size_t _byte_idx = 0;
 };
-DummySerial HwSerial;
-#else
-HardwareSerial HwSerial(HwSerialPort);
 #endif
 
 bool Controller::init(bool verboseLogging)
@@ -220,9 +216,18 @@ bool Controller::init(bool verboseLogging)
         return false;
     }
 
-    HwSerial.end(); // make sure the UART will be re-initialized
-    HwSerial.begin(115200, SERIAL_8N1, pin.battery_rx, pin.battery_tx);
-    HwSerial.flush();
+#ifdef JKBMS_DUMMY_SERIAL
+    _upSerial = std::make_unique<DummySerial>();
+#else
+    auto oHwSerialPort = SerialPortManager.allocatePort(_serialPortOwner);
+    if (!oHwSerialPort) { return false; }
+
+    _upSerial = std::make_unique<HardwareSerial>(*oHwSerialPort);
+#endif
+
+    _upSerial->end(); // make sure the UART will be re-initialized
+    _upSerial->begin(115200, SERIAL_8N1, pin.battery_rx, pin.battery_tx);
+    _upSerial->flush();
 
     if (Interface::Transceiver != getInterface()) { return true; }
 
@@ -242,10 +247,12 @@ bool Controller::init(bool verboseLogging)
 
 void Controller::deinit()
 {
-    HwSerial.end();
+    _upSerial->end();
 
     if (_rxEnablePin > 0) { pinMode(_rxEnablePin, INPUT); }
     if (_txEnablePin > 0) { pinMode(_txEnablePin, INPUT); }
+
+    SerialPortManager.freePort(_serialPortOwner);
 }
 
 Controller::Interface Controller::getInterface() const
@@ -296,7 +303,7 @@ void Controller::sendRequest(uint8_t pollInterval)
         return announceStatus(Status::WaitingForPollInterval);
     }
 
-    if (!HwSerial.availableForWrite()) {
+    if (!_upSerial->availableForWrite()) {
         return announceStatus(Status::HwSerialNotAvailableForWrite);
     }
 
@@ -307,10 +314,10 @@ void Controller::sendRequest(uint8_t pollInterval)
         digitalWrite(_txEnablePin, HIGH); // enable transmission
     }
 
-    HwSerial.write(readAll.data(), readAll.size());
+    _upSerial->write(readAll.data(), readAll.size());
 
     if (Interface::Transceiver == getInterface()) {
-        HwSerial.flush();
+        _upSerial->flush();
         digitalWrite(_rxEnablePin, LOW); // enable reception
         digitalWrite(_txEnablePin, LOW); // disable transmission (free the bus)
     }
@@ -326,8 +333,8 @@ void Controller::loop()
     CONFIG_T& config = Configuration.get();
     uint8_t pollInterval = config.Battery.JkBmsPollingInterval;
 
-    while (HwSerial.available()) {
-        rxData(HwSerial.read());
+    while (_upSerial->available()) {
+        rxData(_upSerial->read());
     }
 
     sendRequest(pollInterval);

include/Battery.h
@@ -14,7 +14,6 @@ public:
     virtual void deinit() = 0;
     virtual void loop() = 0;
     virtual std::shared_ptr<BatteryStats> getStats() const = 0;
-    virtual int usedHwUart() const { return -1; } // -1 => no HW UART used
 };
 
 class BatteryClass {

include/SerialPortManager.h
@@ -1,29 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#include <map>
+#include <array>
+#include <optional>
+#include <string>
 
 class SerialPortManagerClass {
 public:
     void init();
-    bool allocateMpptPort(uint8_t port);
-    bool allocateBatteryPort(uint8_t port);
-    void invalidateBatteryPort();
-    void invalidateMpptPorts();
+
+    std::optional<uint8_t> allocatePort(std::string const& owner);
+    void freePort(std::string const& owner);
 
 private:
-    enum class Owner {
-        Console,
-        Battery,
-        MPPT
-    };
-
-    std::map<uint8_t, Owner> allocatedPorts;
-
-    bool allocatePort(uint8_t port, Owner owner);
-    void invalidate(Owner owner);
-
-    static const char* print(Owner owner);
+    // the amount of hardare UARTs available on supported ESP32 chips
+    static size_t constexpr _num_controllers = 3;
+    std::array<std::string, _num_controllers> _ports = { "" };
 };
 
 extern SerialPortManagerClass SerialPortManager;

include/VictronMppt.h
@@ -55,8 +55,9 @@ private:
     using controller_t = std::unique_ptr<VeDirectMpptController>;
     std::vector<controller_t> _controllers;
 
+    std::vector<String> _serialPortOwners;
     bool initController(int8_t rx, int8_t tx, bool logging,
-        uint8_t instance, uint8_t hwSerialPort);
+        uint8_t instance);
 };
 
 extern VictronMpptClass VictronMppt;

include/VictronSmartShunt.h
@@ -6,13 +6,13 @@
 class VictronSmartShunt : public BatteryProvider {
 public:
     bool init(bool verboseLogging) final;
-    void deinit() final { }
+    void deinit() final;
     void loop() final;
     std::shared_ptr<BatteryStats> getStats() const final { return _stats; }
-    int usedHwUart() const final { return _hwSerialPort; }
 
 private:
-    static uint8_t constexpr _hwSerialPort = ((ARDUINO_USB_CDC_ON_BOOT != 1)?2:0);
+    static char constexpr _serialPortOwner[] = "SmartShunt";
+
     uint32_t _lastUpdate = 0;
     std::shared_ptr<VictronSmartShuntStats> _stats =
         std::make_shared<VictronSmartShuntStats>();

src/Battery.cpp
@@ -5,7 +5,6 @@
 #include "JkBmsController.h"
 #include "VictronSmartShunt.h"
 #include "MqttBattery.h"
-#include "SerialPortManager.h"
 
 BatteryClass Battery;
 
@@ -39,7 +38,6 @@ void BatteryClass::updateSettings()
         _upProvider->deinit();
         _upProvider = nullptr;
     }
-    SerialPortManager.invalidateBatteryPort();
 
     CONFIG_T& config = Configuration.get();
     if (!config.Battery.Enabled) { return; }
@@ -64,18 +62,7 @@ void BatteryClass::updateSettings()
             return;
     }
 
-    // port is -1 if provider is neither JK BMS nor SmartShunt. otherwise, port
-    // is 2, unless running on ESP32-S3 with USB CDC, then port is 0.
-    int port = _upProvider->usedHwUart();
-    if (port >= 0 && !SerialPortManager.allocateBatteryPort(port)) {
-        _upProvider = nullptr;
-        return;
-    }
-
-    if (!_upProvider->init(verboseLogging)) {
-        SerialPortManager.invalidateBatteryPort();
-        _upProvider = nullptr;
-    }
+    if (!_upProvider->init(verboseLogging)) { _upProvider = nullptr; }
 }
 
 void BatteryClass::loop()

commit 034026f78274404d28837b8c02c5cb4fa09cef4a

restore JK BMS dummy implementation
broke in be41e6b9. was unusable as the complete type of DummySerial has
to be available in the JK BMS controller header when defining the
unique_ptr managing the dummy instance. this problem is solved by moving
the whole dummy class into its own header.

include/JkBmsController.h
@@ -6,6 +6,7 @@
 
 #include "Battery.h"
 #include "JkBmsSerialMessage.h"
+#include "JkBmsDummy.h"
 
 //#define JKBMS_DUMMY_SERIAL
 

include/JkBmsDummy.h
@@ -1,0 +1,196 @@
+#pragma once
+
+#include <Arduino.h>
+#include <vector>
+#include "MessageOutput.h"
+
+namespace JkBms {
+
+class DummySerial {
+    public:
+        DummySerial() = default;
+        void begin(uint32_t, uint32_t, int8_t, int8_t) {
+            MessageOutput.println("JK BMS Dummy Serial: begin()");
+        }
+        void end() { MessageOutput.println("JK BMS Dummy Serial: end()"); }
+        void flush() { }
+        bool availableForWrite() const { return true; }
+        size_t write(const uint8_t *buffer, size_t size) {
+            MessageOutput.printf("JK BMS Dummy Serial: write(%d Bytes)\r\n", size);
+            _byte_idx = 0;
+            _msg_idx = (_msg_idx + 1) % _data.size();
+            return size;
+        }
+        bool available() const {
+            return _byte_idx < _data[_msg_idx].size();
+        }
+        int read() {
+            if (_byte_idx >= _data[_msg_idx].size()) { return 0; }
+            return _data[_msg_idx][_byte_idx++];
+        }
+
+    private:
+        std::vector<std::vector<uint8_t>> const _data =
+        {
+            {
+                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
+                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0xfb,
+                0x02, 0x0c, 0xfb, 0x03, 0x0c, 0xfb, 0x04, 0x0c,
+                0xfb, 0x05, 0x0c, 0xfb, 0x06, 0x0c, 0xfb, 0x07,
+                0x0c, 0xfb, 0x08, 0x0c, 0xf7, 0x09, 0x0d, 0x01,
+                0x0a, 0x0c, 0xf9, 0x0b, 0x0c, 0xfb, 0x0c, 0x0c,
+                0xfb, 0x0d, 0x0c, 0xfb, 0x0e, 0x0c, 0xf8, 0x0f,
+                0x0c, 0xf9, 0x10, 0x0c, 0xfb, 0x80, 0x00, 0x1a,
+                0x81, 0x00, 0x12, 0x82, 0x00, 0x12, 0x83, 0x14,
+                0xc3, 0x84, 0x83, 0xf4, 0x85, 0x2e, 0x86, 0x02,
+                0x87, 0x00, 0x15, 0x89, 0x00, 0x00, 0x13, 0x52,
+                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x00, 0x8c, 0x00,
+                0x03, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
+                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
+                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
+                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
+                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
+                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
+                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
+                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
+                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
+                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
+                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
+                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
+                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
+                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
+                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
+                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
+                0x33, 0x30, 0x36, 0xb6, 0x00, 0x01, 0x4a, 0xc3,
+                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
+                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
+                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
+                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
+                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
+                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
+                0x00, 0x53, 0xbb
+            },
+            {
+                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
+                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0xc0,
+                0x02, 0x0c, 0xc1, 0x03, 0x0c, 0xc0, 0x04, 0x0c,
+                0xc4, 0x05, 0x0c, 0xc4, 0x06, 0x0c, 0xc2, 0x07,
+                0x0c, 0xc2, 0x08, 0x0c, 0xc1, 0x09, 0x0c, 0xba,
+                0x0a, 0x0c, 0xc1, 0x0b, 0x0c, 0xc2, 0x0c, 0x0c,
+                0xc2, 0x0d, 0x0c, 0xc2, 0x0e, 0x0c, 0xc4, 0x0f,
+                0x0c, 0xc2, 0x10, 0x0c, 0xc1, 0x80, 0x00, 0x1b,
+                0x81, 0x00, 0x1b, 0x82, 0x00, 0x1a, 0x83, 0x14,
+                0x68, 0x84, 0x03, 0x70, 0x85, 0x3c, 0x86, 0x02,
+                0x87, 0x00, 0x19, 0x89, 0x00, 0x00, 0x16, 0x86,
+                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x00, 0x8c, 0x00,
+                0x07, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
+                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
+                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
+                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
+                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
+                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
+                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
+                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
+                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
+                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
+                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
+                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
+                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
+                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
+                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
+                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
+                0x33, 0x30, 0x36, 0xb6, 0x00, 0x01, 0x7f, 0x2a,
+                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
+                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
+                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
+                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
+                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
+                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
+                0x00, 0x4f, 0xc1
+            },
+            {
+                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
+                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0x13,
+                0x02, 0x0c, 0x12, 0x03, 0x0c, 0x0f, 0x04, 0x0c,
+                0x15, 0x05, 0x0c, 0x0d, 0x06, 0x0c, 0x13, 0x07,
+                0x0c, 0x16, 0x08, 0x0c, 0x13, 0x09, 0x0b, 0xdb,
+                0x0a, 0x0b, 0xf6, 0x0b, 0x0c, 0x17, 0x0c, 0x0b,
+                0xf5, 0x0d, 0x0c, 0x16, 0x0e, 0x0c, 0x1a, 0x0f,
+                0x0c, 0x1b, 0x10, 0x0c, 0x1c, 0x80, 0x00, 0x18,
+                0x81, 0x00, 0x18, 0x82, 0x00, 0x18, 0x83, 0x13,
+                0x49, 0x84, 0x00, 0x00, 0x85, 0x00, 0x86, 0x02,
+                0x87, 0x00, 0x23, 0x89, 0x00, 0x00, 0x20, 0x14,
+                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x08, 0x8c, 0x00,
+                0x05, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
+                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
+                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
+                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
+                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
+                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
+                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
+                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
+                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
+                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
+                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
+                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
+                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
+                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
+                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
+                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
+                0x33, 0x30, 0x36, 0xb6, 0x00, 0x02, 0x17, 0x10,
+                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
+                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
+                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
+                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
+                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
+                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
+                0x00, 0x45, 0xce
+            },
+            {
+                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
+                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0x07,
+                0x02, 0x0c, 0x0a, 0x03, 0x0c, 0x0b, 0x04, 0x0c,
+                0x08, 0x05, 0x0c, 0x05, 0x06, 0x0c, 0x0b, 0x07,
+                0x0c, 0x07, 0x08, 0x0c, 0x0a, 0x09, 0x0c, 0x08,
+                0x0a, 0x0c, 0x06, 0x0b, 0x0c, 0x0a, 0x0c, 0x0c,
+                0x05, 0x0d, 0x0c, 0x0a, 0x0e, 0x0c, 0x0a, 0x0f,
+                0x0c, 0x0a, 0x10, 0x0c, 0x0a, 0x80, 0x00, 0x06,
+                0x81, 0x00, 0x03, 0x82, 0x00, 0x03, 0x83, 0x13,
+                0x40, 0x84, 0x00, 0x00, 0x85, 0x29, 0x86, 0x02,
+                0x87, 0x00, 0x01, 0x89, 0x00, 0x00, 0x01, 0x0a,
+                0x8a, 0x00, 0x10, 0x8b, 0x02, 0x00, 0x8c, 0x00,
+                0x02, 0x8e, 0x16, 0x80, 0x8f, 0x10, 0x40, 0x90,
+                0x0e, 0x10, 0x91, 0x0d, 0xde, 0x92, 0x00, 0x05,
+                0x93, 0x0a, 0x28, 0x94, 0x0a, 0x5a, 0x95, 0x00,
+                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
+                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
+                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
+                0x9e, 0x00, 0x5a, 0x9f, 0x00, 0x50, 0xa0, 0x00,
+                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
+                0x00, 0x37, 0xa4, 0x00, 0x37, 0xa5, 0x00, 0x03,
+                0xa6, 0x00, 0x05, 0xa7, 0xff, 0xec, 0xa8, 0xff,
+                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
+                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
+                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
+                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
+                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
+                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
+                0x33, 0x30, 0x36, 0xb6, 0x00, 0x03, 0xb7, 0x2d,
+                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
+                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
+                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
+                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
+                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
+                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
+                0x00, 0x41, 0x7b
+            }
+        };
+        size_t _msg_idx = 0;
+        size_t _byte_idx = 0;
+};
+
+} /* namespace JkBms */

src/JkBmsController.cpp
@@ -10,195 +10,6 @@
 
 namespace JkBms {
 
-#ifdef JKBMS_DUMMY_SERIAL
-class DummySerial {
-    public:
-        DummySerial() = default;
-        void begin(uint32_t, uint32_t, int8_t, int8_t) {
-            MessageOutput.println("JK BMS Dummy Serial: begin()");
-        }
-        void end() { MessageOutput.println("JK BMS Dummy Serial: end()"); }
-        void flush() { }
-        bool availableForWrite() const { return true; }
-        size_t write(const uint8_t *buffer, size_t size) {
-            MessageOutput.printf("JK BMS Dummy Serial: write(%d Bytes)\r\n", size);
-            _byte_idx = 0;
-            _msg_idx = (_msg_idx + 1) % _data.size();
-            return size;
-        }
-        bool available() const {
-            return _byte_idx < _data[_msg_idx].size();
-        }
-        int read() {
-            if (_byte_idx >= _data[_msg_idx].size()) { return 0; }
-            return _data[_msg_idx][_byte_idx++];
-        }
-
-    private:
-        std::vector<std::vector<uint8_t>> const _data =
-        {
-            {
-                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
-                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0xfb,
-                0x02, 0x0c, 0xfb, 0x03, 0x0c, 0xfb, 0x04, 0x0c,
-                0xfb, 0x05, 0x0c, 0xfb, 0x06, 0x0c, 0xfb, 0x07,
-                0x0c, 0xfb, 0x08, 0x0c, 0xf7, 0x09, 0x0d, 0x01,
-                0x0a, 0x0c, 0xf9, 0x0b, 0x0c, 0xfb, 0x0c, 0x0c,
-                0xfb, 0x0d, 0x0c, 0xfb, 0x0e, 0x0c, 0xf8, 0x0f,
-                0x0c, 0xf9, 0x10, 0x0c, 0xfb, 0x80, 0x00, 0x1a,
-                0x81, 0x00, 0x12, 0x82, 0x00, 0x12, 0x83, 0x14,
-                0xc3, 0x84, 0x83, 0xf4, 0x85, 0x2e, 0x86, 0x02,
-                0x87, 0x00, 0x15, 0x89, 0x00, 0x00, 0x13, 0x52,
-                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x00, 0x8c, 0x00,
-                0x03, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
-                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
-                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
-                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
-                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
-                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
-                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
-                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
-                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
-                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
-                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
-                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
-                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
-                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
-                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
-                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
-                0x33, 0x30, 0x36, 0xb6, 0x00, 0x01, 0x4a, 0xc3,
-                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
-                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
-                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
-                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
-                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
-                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
-                0x00, 0x53, 0xbb
-            },
-            {
-                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
-                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0xc0,
-                0x02, 0x0c, 0xc1, 0x03, 0x0c, 0xc0, 0x04, 0x0c,
-                0xc4, 0x05, 0x0c, 0xc4, 0x06, 0x0c, 0xc2, 0x07,
-                0x0c, 0xc2, 0x08, 0x0c, 0xc1, 0x09, 0x0c, 0xba,
-                0x0a, 0x0c, 0xc1, 0x0b, 0x0c, 0xc2, 0x0c, 0x0c,
-                0xc2, 0x0d, 0x0c, 0xc2, 0x0e, 0x0c, 0xc4, 0x0f,
-                0x0c, 0xc2, 0x10, 0x0c, 0xc1, 0x80, 0x00, 0x1b,
-                0x81, 0x00, 0x1b, 0x82, 0x00, 0x1a, 0x83, 0x14,
-                0x68, 0x84, 0x03, 0x70, 0x85, 0x3c, 0x86, 0x02,
-                0x87, 0x00, 0x19, 0x89, 0x00, 0x00, 0x16, 0x86,
-                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x00, 0x8c, 0x00,
-                0x07, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
-                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
-                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
-                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
-                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
-                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
-                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
-                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
-                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
-                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
-                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
-                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
-                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
-                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
-                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
-                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
-                0x33, 0x30, 0x36, 0xb6, 0x00, 0x01, 0x7f, 0x2a,
-                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
-                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
-                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
-                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
-                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
-                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
-                0x00, 0x4f, 0xc1
-            },
-            {
-                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
-                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0x13,
-                0x02, 0x0c, 0x12, 0x03, 0x0c, 0x0f, 0x04, 0x0c,
-                0x15, 0x05, 0x0c, 0x0d, 0x06, 0x0c, 0x13, 0x07,
-                0x0c, 0x16, 0x08, 0x0c, 0x13, 0x09, 0x0b, 0xdb,
-                0x0a, 0x0b, 0xf6, 0x0b, 0x0c, 0x17, 0x0c, 0x0b,
-                0xf5, 0x0d, 0x0c, 0x16, 0x0e, 0x0c, 0x1a, 0x0f,
-                0x0c, 0x1b, 0x10, 0x0c, 0x1c, 0x80, 0x00, 0x18,
-                0x81, 0x00, 0x18, 0x82, 0x00, 0x18, 0x83, 0x13,
-                0x49, 0x84, 0x00, 0x00, 0x85, 0x00, 0x86, 0x02,
-                0x87, 0x00, 0x23, 0x89, 0x00, 0x00, 0x20, 0x14,
-                0x8a, 0x00, 0x10, 0x8b, 0x00, 0x08, 0x8c, 0x00,
-                0x05, 0x8e, 0x16, 0x80, 0x8f, 0x12, 0xc0, 0x90,
-                0x0e, 0x10, 0x91, 0x0c, 0xda, 0x92, 0x00, 0x05,
-                0x93, 0x0b, 0xb8, 0x94, 0x0c, 0x80, 0x95, 0x00,
-                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
-                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
-                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
-                0x9e, 0x00, 0x64, 0x9f, 0x00, 0x50, 0xa0, 0x00,
-                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
-                0x00, 0x46, 0xa4, 0x00, 0x46, 0xa5, 0x00, 0x00,
-                0xa6, 0x00, 0x02, 0xa7, 0xff, 0xec, 0xa8, 0xff,
-                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
-                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
-                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
-                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
-                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
-                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
-                0x33, 0x30, 0x36, 0xb6, 0x00, 0x02, 0x17, 0x10,
-                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
-                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
-                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
-                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
-                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
-                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
-                0x00, 0x45, 0xce
-            },
-            {
-                0x4e, 0x57, 0x01, 0x21, 0x00, 0x00, 0x00, 0x00,
-                0x06, 0x00, 0x01, 0x79, 0x30, 0x01, 0x0c, 0x07,
-                0x02, 0x0c, 0x0a, 0x03, 0x0c, 0x0b, 0x04, 0x0c,
-                0x08, 0x05, 0x0c, 0x05, 0x06, 0x0c, 0x0b, 0x07,
-                0x0c, 0x07, 0x08, 0x0c, 0x0a, 0x09, 0x0c, 0x08,
-                0x0a, 0x0c, 0x06, 0x0b, 0x0c, 0x0a, 0x0c, 0x0c,
-                0x05, 0x0d, 0x0c, 0x0a, 0x0e, 0x0c, 0x0a, 0x0f,
-                0x0c, 0x0a, 0x10, 0x0c, 0x0a, 0x80, 0x00, 0x06,
-                0x81, 0x00, 0x03, 0x82, 0x00, 0x03, 0x83, 0x13,
-                0x40, 0x84, 0x00, 0x00, 0x85, 0x29, 0x86, 0x02,
-                0x87, 0x00, 0x01, 0x89, 0x00, 0x00, 0x01, 0x0a,
-                0x8a, 0x00, 0x10, 0x8b, 0x02, 0x00, 0x8c, 0x00,
-                0x02, 0x8e, 0x16, 0x80, 0x8f, 0x10, 0x40, 0x90,
-                0x0e, 0x10, 0x91, 0x0d, 0xde, 0x92, 0x00, 0x05,
-                0x93, 0x0a, 0x28, 0x94, 0x0a, 0x5a, 0x95, 0x00,
-                0x05, 0x96, 0x01, 0x2c, 0x97, 0x00, 0x28, 0x98,
-                0x01, 0x2c, 0x99, 0x00, 0x28, 0x9a, 0x00, 0x1e,
-                0x9b, 0x0b, 0xb8, 0x9c, 0x00, 0x0a, 0x9d, 0x01,
-                0x9e, 0x00, 0x5a, 0x9f, 0x00, 0x50, 0xa0, 0x00,
-                0x64, 0xa1, 0x00, 0x64, 0xa2, 0x00, 0x14, 0xa3,
-                0x00, 0x37, 0xa4, 0x00, 0x37, 0xa5, 0x00, 0x03,
-                0xa6, 0x00, 0x05, 0xa7, 0xff, 0xec, 0xa8, 0xff,
-                0xf6, 0xa9, 0x10, 0xaa, 0x00, 0x00, 0x00, 0xe6,
-                0xab, 0x01, 0xac, 0x01, 0xad, 0x04, 0x4d, 0xae,
-                0x01, 0xaf, 0x00, 0xb0, 0x00, 0x0a, 0xb1, 0x14,
-                0xb2, 0x32, 0x32, 0x31, 0x31, 0x38, 0x37, 0x00,
-                0x00, 0x00, 0x00, 0xb3, 0x00, 0xb4, 0x62, 0x65,
-                0x6b, 0x69, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x32,
-                0x33, 0x30, 0x36, 0xb6, 0x00, 0x03, 0xb7, 0x2d,
-                0xb7, 0x31, 0x31, 0x2e, 0x58, 0x57, 0x5f, 0x53,
-                0x31, 0x31, 0x2e, 0x32, 0x36, 0x32, 0x48, 0x5f,
-                0xb8, 0x00, 0xb9, 0x00, 0x00, 0x00, 0xe6, 0xba,
-                0x62, 0x65, 0x6b, 0x69, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x4a, 0x4b, 0x5f, 0x42,
-                0x31, 0x41, 0x32, 0x34, 0x53, 0x31, 0x35, 0x50,
-                0xc0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x00,
-                0x00, 0x41, 0x7b
-            }
-        };
-        size_t _msg_idx = 0;
-        size_t _byte_idx = 0;
-};
-#endif
-
 bool Controller::init(bool verboseLogging)
 {
     _verboseLogging = verboseLogging;