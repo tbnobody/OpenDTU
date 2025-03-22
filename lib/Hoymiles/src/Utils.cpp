a17cf532d4a2c90c94260e3dc223609938423be5
Thomas Basler <thomas@familie-basler.net>
2025-03-22 14:45:51 +0100

    Simplify code of weekday calculation

--- lib/Hoymiles/src/Utils.cpp
+++ lib/Hoymiles/src/Utils.cpp
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (C) 2023 Thomas Basler and others
+ * Copyright (C) 2023-2025 Thomas Basler and others
  */
 #include "Utils.h"
 #include <time.h>
 
 uint8_t Utils::getWeekDay()
 {
-    time_t raw;
-    struct tm info;
-    time(&raw);
-    localtime_r(&raw, &info);
-    return info.tm_mday;
+    time_t now = time(NULL);
+    struct tm tm = *localtime(&now);
+    return tm.tm_mday;
 }
