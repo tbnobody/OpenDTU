8ad51f5870fe5d81ceaed9f2d400afa6e77422f3
Bernhard Kirchen <schlimmchen@posteo.net>
2025-06-27 22:01:13 +0200

    syslog: implement rate limiting
    enabling a lot of logging messages, e.g., by setting verbose as the
    default level, can make the system unresponsive if syslogging is
    enabled. to protect against this, a rate limiting algorithm is
    implemented in the syslogger.

--- include/SyslogLogger.h
+++ include/SyslogLogger.h
@@ -20,6 +20,7 @@ private:
         return _address != INADDR_NONE;
     }
     static uint8_t calculatePrival(uint8_t facility, char errorCode);
+    bool consumeToken();
 
     Task _loopTask;
     std::mutex _mutex;
@@ -30,6 +31,14 @@ private:
     String _header;
     uint16_t _port;
     bool _enabled;
+
+    static constexpr uint32_t RATE_LIMIT_WINDOW_MS = 1000;
+    static constexpr size_t RATE_LIMIT_MAX_TOKENS = 100;
+    size_t _available_tokens = RATE_LIMIT_MAX_TOKENS;
+    uint32_t _last_token_refill_millis = 0;
+    size_t _rate_limited_packets = 0;
+    uint32_t _last_rate_limit_warning_millis = 0;
+    static constexpr uint32_t RATE_LIMIT_WARNING_INTERVAL_MS = 1000;
 };
 
 extern SyslogLogger Syslog;
